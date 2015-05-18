56624fe15e6f9e866b4e16ce1f9b245d35132993
Blaine Motsinger <blaine@renderorange.com>
2015-05-18 18:03:05 -0500

    changed name to country.pl, finished logic

--- country.pl
+++ country.pl
@@ -16,27 +16,38 @@ if ($total_syllables < 5 || $total_syllables > 10) { die "warning: Whoa partner,
 if ($total_lines < 1) { die "warning: Ain't no song made of nothing, is it?\n"; } 
 elsif ($total_lines > 50) { die "warning: What, you think this is 'merican Pie or somethin?\n"; }
 
-# the data, categorized by type and number of syllables
+# the data, categorized by number of syllables
 my %words = (
-    '1' => [ 'beer', 'dog', 'bar', 'truck', 'walk', 'ride', 'drive', 'sway' ],
-    '2' => [ 'girlfriend', 'wedding', 'chicken', 'bluejeans', 'dirtroad' ],
-    '3' => [ 'honkeytonk', 'swaggerin', 'saunterin', 'moseyin' ],
+    '1' => ['beer', 'dog', 'bar', 'truck', 'walk', 'ride', 'drive', 'sway'],
+    '2' => ['girlfriend', 'weddin', 'chicken', 'bluejeans', 'dirtroad'],
+    '3' => ['honkeytonk', 'swaggerin', 'saunterin', 'moseyin'],
 );
 
-# build up the sentence
-for (my $line = 0; $line < $total_lines; ++$line) {
-    for (my $syllable = 0; $syllable < $total_syllables; ++$syllable) {
-        my ($random_syllables, $random_word) = get_random_word();
-        print "[$random_syllables] $random_word ";
+# build and output the sentences
+for (my $line = 0; $line < $total_lines; ++$line) {  # loop for the amount of total_lines
+    my $syllable = 0; 
+    while ($syllable < $total_syllables) {  # loop until total_syllables are satisfied
+        my $remaining = $total_syllables - $syllable;
+        my ($random_syllables, $random_word);
+        if ($remaining <= keys %words) {  # if the remaining syllables in the sentence can be satisfied by a specific syllable tier
+            ($random_syllables, $random_word) = get_random_word($remaining);  # then get a word in that specific syllable tier
+        } else {
+            ($random_syllables, $random_word) = get_random_word();  # otherwise get random
+        }
+        $syllable += $random_syllables;
+        print "$random_word ";
     }
     print "\n";
 }
 
 # subs
 sub get_random_word {
-    my $random_syllable = int(rand(scalar keys %words)) + 1;
-    my $random_array_size = scalar @{ $words{ $random_syllable } };
-    my $random_word_number = int(rand($random_array_size - 1));
-    my $random_word = $words{$random_syllable}[ $random_word_number ];
+    my $random_syllable = shift;  # if a number is passed in, get from that syllable tier
+    if (! defined $random_syllable) {
+        $random_syllable = int(rand(scalar keys %words)) + 1;  # otherwise, get from a random syllable tier
+    }
+    my $random_array_size = scalar @{$words{$random_syllable}};
+    my $random_word_number = int(rand($random_array_size));
+    my $random_word = $words{$random_syllable}[$random_word_number];
     return ($random_syllable, $random_word);
 }
